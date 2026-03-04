5d388d35ce237cc7beb98a9553335cf46e26abc3
Vaishnavi D <165470432+Vaishnavi-D-Github@users.noreply.github.com>
2026-03-04 18:20:13 +0530

    Minor spelling changes

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,3 @@
 from .user import User
-from .Workgroup import Workgroup
-from .WorkgroupAssignment import WorkgroupAssignment
+from .workgroup import Workgroup
+from .workgroupAssignment import WorkgroupAssignment
