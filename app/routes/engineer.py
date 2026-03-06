7dfac536421be168ccc4276c08440ac5cc622c72
Vaishnavi-D-Github <vaishnavidsamu@gmail.com>
2026-03-06 07:19:01 +0530

    Updates in Engineer and Fixed bugs in Manager

--- app/routes/engineer.py
+++ app/routes/engineer.py
@@ -1,69 +1,53 @@
-from flask import Blueprint, jsonify, session, render_template, redirect, url_for
+from flask import Blueprint, render_template, session, redirect, url_for, jsonify
+from app.models.user import User
 from app.models.workgroup import Workgroup
 from app.models.workgroupAssignment import WorkgroupAssignment
-from app.models.user import User
 
 engineer = Blueprint("engineer", __name__)
 
 
-@engineer.route("/engineer_dashboard")
+@engineer.route("/engineer/dashboard")
 def engineer_dashboard():
+
     if "user_id" not in session:
         return redirect(url_for("auth.login"))
-    return render_template("engineerDashboard.html")
-
-
-@engineer.route("/api/engineer/stats")
-def engineer_stats():
 
     engineer_id = session["user_id"]
+
+    current_user = User.query.get(engineer_id)
 
     assignments = WorkgroupAssignment.query.filter_by(
         employee_id=engineer_id
     ).all()
 
-    workgroups = [Workgroup.query.get(a.workgroup_id) for a in assignments]
+    workgroups = []
 
-    assigned = len(workgroups)
-    active = len([w for w in workgroups if w.is_completed == "Active"])
-    completed = len([w for w in workgroups if w.is_completed == "Completed"])
-
-    return jsonify({
-        "assigned": assigned,
-        "active": active,
-        "completed": completed
-    })
-
-
-
-@engineer.route("/api/engineer/workgroups")
-def engineer_workgroups():
-
-    engineer_id = session["user_id"]
-
-    assignments = WorkgroupAssignment.query.filter_by(
-        employee_id=engineer_id
-    ).all()
-
-    result = []
+    active = 0
+    completed = 0
 
     for a in assignments:
 
         wg = Workgroup.query.get(a.workgroup_id)
 
-        result.append({
-            "id": wg.id,
-            "name": wg.name,
-            "release_version": wg.release_version,
-            "is_completed": True if wg.is_completed == "Completed" else False,
-            "created_at": wg.created_at
-        })
+        if wg:
 
-    return jsonify(result)
+            workgroups.append(wg)
 
-# LOGOUT
-@engineer.route("/api/auth/logout", methods=["POST"])
+            if wg.is_completed == "Completed":
+                completed += 1
+            else:
+                active += 1
+
+    return render_template(
+        "engineerDashboard.html",
+        current_user=current_user,
+        workgroups=workgroups,
+        total=len(workgroups),
+        active=active,
+        completed=completed
+    )
+
+@engineer.route("/api/auth/logout", methods=["POST","GET"])
 def logout():
     session.clear()
-    return jsonify({"success": True})
-
+    return redirect(url_for("auth.login"))
